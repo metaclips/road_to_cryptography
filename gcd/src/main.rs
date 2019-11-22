9fe5390acd5f1d29c0d468977ba5dd732620eed5
metaclips <utimichael9@gmail.com>
2019-11-22 17:16:18 +0100

    improved gcd algorithm

--- gcd/src/main.rs
+++ gcd/src/main.rs
@@ -2,19 +2,19 @@ use std::io;
 
 fn main() {
     let mut no1 = String::new();
-
+    println!("Please input first number:");
     io::stdin()
         .read_line(&mut no1)
         .expect("Could not read from buffer");
 
     let mut no2 = String::new();
+    println!("Please input second number:");
     io::stdin()
         .read_line(&mut no2)
         .expect("Could not read from buffer");
 
     let no2 = no2.trim();
     let no1 = no1.trim();
-
     let mut no1 = no1.trim().parse::<i32>().expect("invalid input");
     let mut no2 = no2.trim().parse::<i32>().expect("invalid input");
 
@@ -23,18 +23,18 @@ fn main() {
         no2 = no1;
         no1 = initial_number1;
     }
-    let mut answer = 0;
-    euclidean_algorithm(&mut no1, &mut no2, &mut answer);
+
+    euclidean_algorithm(&mut no1, &mut no2);
     println!("The greatest common divisor is {}", no2);
 }
 
-fn euclidean_algorithm(a: &mut i32, b: &mut i32, remainder: &mut i32) {
-    *remainder = *a % *b;
-    if *remainder <= 0 {
+fn euclidean_algorithm(a: &mut i32, b: &mut i32) {
+    let remainder = *a % *b;
+    if remainder <= 0 {
         return;
     }
 
     *a = *b;
-    *b = *remainder;
-    euclidean_algorithm(a, b, remainder)
+    *b = remainder;
+    euclidean_algorithm(a, b)
 }
